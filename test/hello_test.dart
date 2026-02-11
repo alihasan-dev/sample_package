75d14b80f32a25ef6f71554a56cd7e0ea125c98a
Ali Hasan <alihasan.dev@gmail.com>
2026-02-12 01:08:19 +0530

    change package name

--- test/hello_test.dart
+++ test/hello_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sample_package/date_utils.dart';
+import 'package:sample_formatter/date_utils.dart';
 
 void main() {
   test('adds one to input values', () {
